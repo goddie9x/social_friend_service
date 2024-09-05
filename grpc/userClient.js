b8894d02f7db487f006dbcb424ea054472859d0c
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-05 13:26:19 +0700

    refactor: update grpc client communications

--- grpc/userClient.js
+++ grpc/userClient.js
@@ -2,14 +2,17 @@ const grpc = require('@grpc/grpc-js');
 
 const userProto = require('../generated/user_grpc_pb');
 const userMessages = require('../generated/user_pb');
+const { getListUserByIdsGRPCGenerate } = require('../utils/grpc/user');
 
 const userServiceClient = new userProto.UserServiceClient(
     process.env.USER_SERVICE_GRPC_ADDRESS || 'localhost:50051',
     grpc.credentials.createInsecure()
 );
 
+const getListUserByIds = getListUserByIdsGRPCGenerate({ userMessages, userServiceClient });
 
 module.exports = {
     userServiceClient,
-    userMessages
-}
+    userMessages,
+    getListUserByIds
+};
--- models/friend.js
+++ models/friend.js
@@ -3,35 +3,14 @@ const mongoose = require('mongoose');
 const { SenderMustDifferentWithReceiverException } = require('../exceptions/friendExceptions');
 const { TargetNotExistException } = require('../utils/exceptions/commonExceptions');
 const { FRIENDSHIP } = require('../constants/friend');
-const { userServiceClient, userMessages } = require('../grpc/userClient');
+const { getListUserByIds } = require('../grpc/userClient');
 
 const Schema = mongoose.Schema;
 const friendshipArray = Object.values(FRIENDSHIP);
 
-const getUserById = (userId) => {
-    return new Promise((resolve, reject) => {
-        const userIdString = userId.toString();
-        const request = new userMessages.GetUserByIdRequest();
-        request.setId(userIdString);
-
-        userServiceClient.getUserById(request, (error, response) => {
-            if (error) {
-                return reject(error);
-            }
-            const user = {
-                id: response.getId(),
-                username: response.getUsername(),
-            };
-
-            resolve(user);
-        });
-    })
-}
-
 const FriendSchema = new Schema({
     sender: {
         type: Schema.Types.ObjectId,
-        ref: 'User',
         required: true,
     },
     receiver: {
@@ -57,24 +36,16 @@ const FriendSchema = new Schema({
     }
 });
 
-FriendSchema.index({ sender: 1, receiver: 1 });
-FriendSchema.index({ receiver: 1, sender: 1 });
+FriendSchema.index({ sender: 1, receiver: 1, createdAt: -1 });
+FriendSchema.index({ receiver: 1, sender: 1, createdAt: -1 });
 
 FriendSchema.pre('save', async function (next) {
     try {
         if (this.sender.equals(this.receiver)) {
             throw new SenderMustDifferentWithReceiverException();
         }
-        const getSenderUserById = getUserById(this.sender);
-        const getReceiverUserById = getUserById(this.receiver);
-        const [senderResponse, receiverResponse] = await Promise.all(
-            [getSenderUserById,
-                getReceiverUserById]
-        );
-        const senderExists = senderResponse && senderResponse.id;
-        const receiverExists = receiverResponse && receiverResponse.id;
-        
-        if (!senderExists || !receiverExists) {
+        const response = await getListUserByIds([this.sender, this.receiver]);
+        if (!response || !response.users || response.users?.length < 2) {
             throw new TargetNotExistException('Either the sender or the receiver does not exist.');
         }
         next();
